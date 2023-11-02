38f1e4ce811b5d7b4866d4f536e35a6e89167f9d
Pasha Fentisov <pasha16.ua@gmail.com>
2023-11-02 19:59:50 +0200

    Initial commit

---
+++ src/main/java/com/pashonokk/marketplace/MarketPlaceApplication.java
@@ -0,0 +1,20 @@
+package com.pashonokk.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPlaceApplication.class, args);
+    }
+
+    @GetMapping
+    public String homePage() {
+        return "Hello this the home page";
+    }
+}
